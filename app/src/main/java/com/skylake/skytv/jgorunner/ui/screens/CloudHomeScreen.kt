338435fa0bc34c4890b70d8df6c939dde4b5db36
Atanu Roy <123448167+atanuroy22@users.noreply.github.com>
2026-05-15 02:34:39 +0530

    Update subscription message in CloudHomeScreen

--- app/src/main/java/com/skylake/skytv/jgorunner/ui/screens/CloudHomeScreen.kt
+++ app/src/main/java/com/skylake/skytv/jgorunner/ui/screens/CloudHomeScreen.kt
@@ -167,7 +167,7 @@ fun CloudHomeScreen(
                 val expiryText = if (isSubscribed) {
                     "Valid until: ${sdf.format(Date(subExpiry))}"
                 } else {
-                    "🎉 Get Jio+ at special price!"
+                    "🎉 Get Jio+ & Sports at Lowest Price!"
                 }
 
                 Text(
